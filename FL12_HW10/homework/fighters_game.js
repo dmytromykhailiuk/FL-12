6dd7362d53804d1b321885300876b10e5cf761a8
Dmytro Mykhailiuk <dimamykhayluk@gmail.com>
2020-01-17 02:07:26 +0200

    Start working at HW10

---
+++ FL12_HW10/homework/fighters_game.js
@@ -0,0 +1,78 @@
+function Fighter(properties) {
+  const percentageCoef = 101;
+  const maxHp = properties.hp;
+  const combatHistory = {
+    wins: 0,
+    losses: 0
+  };
+
+  this.getName = () => properties.name;
+  this.getDamage = () => properties.damage;
+  this.getStrength = () => properties.strength;
+  this.getAgility = () => properties.agility;
+  this.getHealth = () => properties.hp;
+
+  this.attack = enemy => {
+    if (Math.floor(Math.random() * percentageCoef) > enemy.getStrength() + enemy.getAgility()) {
+      console.log(`${this.getName()} makes ${this.getDamage()} damage to ${enemy.getName()}`);
+      enemy.dealDamage(this.getDamage());
+    } else {
+      console.log(`${this.getName()} attack missed`);
+    }
+  };
+
+  this.logCombatHistory = () => {
+    console.log(`Name: ${this.getName()}, Wins: ${combatHistory.wins}, Losses: ${combatHistory.losses}`);
+  };
+
+  this.heal = healtHp => {
+    properties.hp + healtHp > maxHp ? properties.hp = maxHp : properties.hp += healtHp;
+  };
+
+  this.dealDamage = someDamage => {
+    properties.hp - someDamage < 0 ? properties.hp = 0 : properties.hp -= someDamage;
+  };
+
+  this.addWin = () => {
+    combatHistory.wins++;
+  };
+
+  this.addLoss = () => {
+    combatHistory.losses++;
+  }
+}
+
+function setWinBattle(winer, loser) {
+  console.log(`${winer.getName()} has won!`); 
+  winer.addWin();
+  loser.addLoss();
+}
+
+function battle(fighter1, fighter2) {
+  let stopBattle = false;
+  [fighter1, fighter2].forEach(fighter => {
+    if (!fighter) {
+      console.log(`${fighter.getName()} is dead`);
+      stopBattle = true;
+    }
+  });
+  while (!stopBattle) {
+    fighter1.attack(fighter2);
+    if (!fighter2.getHealth()) {
+      setWinBattle(fighter1, fighter2);
+      stopBattle = true;
+    }
+    if (!stopBattle) {
+      fighter2.attack(fighter1);
+      if (!fighter1.getHealth()) {
+        setWinBattle(fighter2, fighter1);
+        stopBattle = true;
+      }
+    }
+  }
+}
+
+const fighter1 = new Fighter({ name: 'Maximus', damage: 20, hp: 100, strength: 20, agility: 15 });
+const fighter2 = new Fighter({ name: 'Commodus', damage: 25, hp: 90, strength: 25, agility: 20 });
+
+battle(fighter1, fighter2);

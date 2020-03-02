84e34dcf120e2df16b2a97112d7ce505b2419173
Дмитро Михайлюк <dimamykhaylukgmail.com@MacBook-Pro-Dmitro.local>
2020-03-02 02:21:00 +0200

    working at task2 in HW_Webinar_JQuery

--- FL_12_06_homework_JQuery/homework/task2/js/index.js
+++ FL_12_06_homework_JQuery/homework/task2/js/index.js
@@ -1,14 +1,68 @@
-const $list = $(".list");
-const $input = $("#add-input");
-const $add = $("#add-submit");
-
-const todos = [
-  {
-    text: "Buy milk",
-    done: false
-  },
-  {
-    text: "Play with dog",
-    done: true
-  }
+const todos = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : [
+  { text : "Buy milk" , done: false }, { text : "Play with dog" , done: true }
 ];
+
+todos.forEach(el => {
+  createElement(el.text, el.done);
+});
+
+$("#add-submit").click(function(event) {
+  event.preventDefault();
+  const textVal = $("#add-input").val();
+  if (textVal !== "" && !todos.some(el => el.text.toLowerCase() === textVal.toLowerCase())) {
+    createElement(textVal);
+    addEventListners();
+    todos.push({ text : textVal , done: false });
+    $("#add-input").val("");
+    saveData();
+  }
+})
+
+$(".item-remove").click(removeItem);
+
+
+$(".item-text").click(toggleDone);
+
+saveData();
+
+function toggleDone(event) {
+  event.preventDefault();
+  $(event.delegateTarget).toggleClass("done");
+  todos.forEach( el => {
+    if (el.text === $(event.delegateTarget).text()) {
+      el.done = !el.done;
+    }
+  });
+  saveData();
+};
+
+function removeItem(event) {
+  $(event.delegateTarget).parent(".item").remove();
+  todos.forEach( (el, i) => {
+    if (el.text === $(event.delegateTarget).prev().text()) {
+      todos.splice(i, 1);
+    }
+  });
+  saveData();
+};
+
+function createElement(text, isDone = false) {
+  $(".list").append(`
+    <li class="item">
+      <span class="item-text ${isDone ? 'done' : ''}">${text}</span>
+      <button class="item-remove">Remove</button>
+    </li>
+  `);
+};
+
+function addEventListners() {
+  $(".list .item:last-child .item-remove").click(removeItem);
+  $(".list .item:last-child .item-text").click(toggleDone);
+}
+
+function saveData() {
+  localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+
+
